4fa6a8614c31a32bd8762b31d8bbd089ad36b44e
oguuzhansahin <oguuzhansahiin@gmail.com>
2023-02-08 21:41:04 +0300

    add label mapping

---
+++ config.py
@@ -0,0 +1,25 @@
+id2label = {
+    0: "O",
+    1: "B-no",
+    2: "I-no",
+    3: "B-mahalle",
+    4: "I-mahalle",
+    5: "B-ad-soyad",
+    6: "I-ad-soyad",
+    7: "B-kat",
+    8: "I-kat",
+    9: "B-ilce",
+    10: "I-ilce",
+    11: "B-sokak",
+    12: "I-sokak",
+    13: "B-dis kapi no",
+    14: "I-dis kapi no",
+    15: "B-Apartman/Site",
+    16: "I-Apartman/Site",
+    17: "B-ic kapi no",
+    18: "I-ic kapi no",
+    19: "B-il",
+    20: "I-il"
+}
+
+label2id = {label: id for id, label in id2label.items()}
