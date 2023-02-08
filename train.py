ff304de09c6e6e79734dea12286edfed3c3f4124
oguuzhansahin <oguuzhansahiin@gmail.com>
2023-02-08 21:48:33 +0300

    Update train.py

--- train.py
+++ train.py
@@ -26,12 +26,6 @@ from transformers.trainer_utils import get_last_checkpoint
 from transformers.utils import check_min_version, send_example_telemetry
 from transformers.utils.versions import require_version
 
-
-# Will error if the minimal version of Transformers is not installed. Remove at your own risks.
-#check_min_version("4.27.0.dev0")
-
-require_version("datasets>=1.8.0", "To fix: pip install -r examples/pytorch/token-classification/requirements.txt")
-
 logger = logging.getLogger(__name__)
 
 @dataclass
